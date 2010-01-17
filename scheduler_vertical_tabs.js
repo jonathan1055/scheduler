8d7f265e9e1ae216e97577771a3b12e844d189d6
Eric Schaefer <eric.schaefer@ericschaefer.org>
2010-01-17 16:24:33 +0000

    #657206 by Dave Reid: Enhancement of vertical tab behavior

--- scheduler_vertical_tabs.js
+++ scheduler_vertical_tabs.js
@@ -5,10 +5,13 @@ Drupal.verticalTabs = Drupal.verticalTabs || {};
 Drupal.verticalTabs.scheduler_settings = function() {
   var vals = [];
   if ($('#edit-publish-on').val() || $('#edit-publish-on-datepicker-popup-0').val()) {
-	vals.push(Drupal.t('Scheduled for publishing'));
+	  vals.push(Drupal.t('Scheduled for publishing'));
   }
   if ($('#edit-unpublish-on').val() || $('#edit-unpublish-on-datepicker-popup-0').val()) {
-	vals.push(Drupal.t('Scheduled for unpublishing'));
+	  vals.push(Drupal.t('Scheduled for unpublishing'));
+  }
+  if (!vals.length) {
+    vals.push(Drupal.t('Not scheduled'));
   }
   return vals.join(', ');
 }
