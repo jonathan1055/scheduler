0fb9ebf16970fb6de8caa0d2b23744045241957b
Eric Schaefer <eric.schaefer@ericschaefer.org>
2011-01-23 08:19:09 +0000

    bug #1035408 by jonathan1055: The vertical fieldset in the node form is not showing the scheduling state

--- scheduler_vertical_tabs.js
+++ scheduler_vertical_tabs.js
@@ -1,17 +1,26 @@
 // $Id$
 
-Drupal.verticalTabs = Drupal.verticalTabs || {};
+(function ($) {
 
-Drupal.verticalTabs.scheduler_settings = function() {
-  var vals = [];
-  if ($('#edit-publish-on').val() || $('#edit-publish-on-datepicker-popup-0').val()) {
-	  vals.push(Drupal.t('Scheduled for publishing'));
+/**
+ * Provide the summary information for the block settings vertical tabs.
+ */
+Drupal.behaviors.scheduler_settings = {
+  attach: function (context) {
+    $('fieldset#edit-scheduler-settings', context).drupalSetSummary(function(context) {
+      var vals = [];
+      if ($('#edit-publish-on').val() || $('#edit-publish-on-datepicker-popup-0').val()) {
+        vals.push(Drupal.t('Scheduled for publishing'));
+      }
+      if ($('#edit-unpublish-on').val() || $('#edit-unpublish-on-datepicker-popup-0').val()) {
+        vals.push(Drupal.t('Scheduled for unpublishing'));
+      }
+      if (!vals.length) {
+        vals.push(Drupal.t('Not scheduled'));
+      }
+      return vals.join('<br/>');
+    });
   }
-  if ($('#edit-unpublish-on').val() || $('#edit-unpublish-on-datepicker-popup-0').val()) {
-	  vals.push(Drupal.t('Scheduled for unpublishing'));
-  }
-  if (!vals.length) {
-    vals.push(Drupal.t('Not scheduled'));
-  }
-  return vals.join(', ');
-}
+};
+
+})(jQuery);
