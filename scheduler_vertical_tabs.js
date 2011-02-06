ecbd77d4b54b584b7e6a4d6021500e9ca02009f6
Eric Schaefer <eric.schaefer@ericschaefer.org>
2011-02-06 19:53:51 +0000

    #1052922 by jonathan1055: Add summary info to Content Type vertical tabs

--- scheduler_vertical_tabs.js
+++ scheduler_vertical_tabs.js
@@ -3,11 +3,12 @@
 (function ($) {
 
 /**
- * Provide the summary information for the block settings vertical tabs.
+ * Provide summary information for vertical tabs.
  */
 Drupal.behaviors.scheduler_settings = {
   attach: function (context) {
-    $('fieldset#edit-scheduler-settings', context).drupalSetSummary(function(context) {
+	// Provide summary when editting a node.
+	$('fieldset#edit-scheduler-settings', context).drupalSetSummary(function(context) {
       var vals = [];
       if ($('#edit-publish-on').val() || $('#edit-publish-on-datepicker-popup-0').val()) {
         vals.push(Drupal.t('Scheduled for publishing'));
@@ -20,6 +21,19 @@ Drupal.behaviors.scheduler_settings = {
       }
       return vals.join('<br/>');
     });
+
+    // Provide summary during content type configuration.
+    $('fieldset#edit-scheduler', context).drupalSetSummary(function(context) {
+      var vals = [];
+      if ($('#edit-scheduler-publish-enable', context).is(':checked')) {
+        vals.push(Drupal.t('Publishing enabled'));
+      }
+      if ($('#edit-scheduler-unpublish-enable', context).is(':checked')) {
+        vals.push(Drupal.t('Unpublishing enabled'));
+      }
+      return vals.join('<br/>');
+    });
+
   }
 };
 
