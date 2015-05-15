a1a0f3ded5c7b6e83e28be76e62bf48aa578b5c6
Pieter Frenssen <pieter@frenssen.be>
2015-05-15 12:55:29 +0200

    Issue #2468819 by pfrenssen: Remove theming workarounds.

--- js/scheduler_vertical_tabs.js
+++ js/scheduler_vertical_tabs.js
@@ -11,13 +11,6 @@
 Drupal.behaviors.scheduler_settings = {
   attach: function (context) {
 
-    // Add the theme name as an additional class to the vertical-tabs div. This
-    // can then be used in scheduler.css to rectify the style for collapsible
-    // fieldsets where different themes need slightly different fixes. The theme
-    // is available in ajaxPageState.
-    var theme = drupalSettings.ajaxPageState['theme'];
-    $('div.vertical-tabs').addClass(theme);
-
     // Provide summary when editing a node.
     $('fieldset#edit-scheduler-settings', context).drupalSetSummary(function(context) {
       var vals = [];
