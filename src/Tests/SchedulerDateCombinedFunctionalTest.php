b6da257d0974f77f08d10e1c1c8f4fead1e0a268
jonathan1055 <jonathan1055@92645.no-reply.drupal.org>
2015-12-06 15:29:08 +0000

    Issue #2594615 by jonathan1055: Date tests - syntax, thirdPartySetting, Edit form (37 passes, 6 fails)

--- src/Tests/SchedulerDateCombinedFunctionalTest.php
+++ src/Tests/SchedulerDateCombinedFunctionalTest.php
@@ -2,14 +2,14 @@
 
 /**
  * @file
- * Contains
- *   \Drupal\scheduler\Tests\ScedulerDateCombineFunctionalTest
+ * Contains \Drupal\scheduler\Tests\ScedulerDateCombineFunctionalTest
  */
 
 namespace Drupal\scheduler\Tests;
 
 use Drupal\Component\Utility\SafeMarkup;
 use Drupal\node\Entity\Node;
+use Drupal\node\Entity\NodeType;
 use Drupal\node\NodeInterface;
 use Drupal\simpletest\WebTestBase;
 
@@ -30,13 +30,21 @@ class SchedulerDateCombinedFunctionalTest extends SchedulerTestBase {
    */
   public function setUp() {
     parent::setUp();
-    $config = $this->config('scheduler.settings');
 
     // Create a 'Basic Page' content type.
-    $this->drupalCreateContentType(array('type' => 'page', 'name' => t('Basic page')));
+    $this->drupalCreateContentType(['type' => 'page', 'name' => t('Basic page')]);
+    ### @TODO the string 'page' is hard-coded eleven times in this file (so far)
+    ### @TODO Could make it a variable, which would allow future testing of other entity types?
+
+    // Add scheduler functionality to the page node type.
+    /** @var NodeTypeInterface $node_type */
+    $node_type = NodeType::load('page');
+    $node_type->setThirdPartySetting('scheduler', 'publish_enable', TRUE);
+    $node_type->setThirdPartySetting('scheduler', 'unpublish_enable', TRUE);
+    $node_type->save();
 
     // Create an administrator user.
-    $this->adminUser = $this->drupalCreateUser(array(
+    $this->adminUser = $this->drupalCreateUser([
       'access content',
       'administer scheduler',
       'create page content',
@@ -45,11 +53,7 @@ class SchedulerDateCombinedFunctionalTest extends SchedulerTestBase {
       'view own unpublished content',
       'administer nodes',
       'schedule (un)publishing of nodes',
-    ));
-
-    // Add scheduler functionality to the page node type.
-    $config->set('scheduler_publish_enable_page', 1);
-    $config->set('scheduler_unpublish_enable_page', 1);
+    ]);
   }
 
   /**
@@ -61,20 +65,20 @@ class SchedulerDateCombinedFunctionalTest extends SchedulerTestBase {
     // Check that the correct default time is added to the scheduled date.
     // For testing we use an offset of 6 hours 30 minutes (23400 seconds).
     $edit = array(
-      'scheduler_date_format' => 'Y-m-d H:i:s',
-      'scheduler_allow_date_only' => TRUE,
-      'scheduler_default_time' => '6:30',
+      'date_format' => 'Y-m-d H:i:s',
+      'allow_date_only' => TRUE,
+      'default_time' => '6:30',
     );
-    $this->drupalPost('admin/config/content/scheduler', $edit, t('Save configuration'));
+    $this->drupalPostForm('admin/config/content/scheduler', $edit, t('Save configuration'));
     $this->assertDefaultTime();
 
     // Check that it is not possible to enter a date format without a time if
     // the 'date only' option is not enabled.
     $edit = array(
-      'scheduler_date_format' => 'Y-m-d',
-      'scheduler_allow_date_only' => FALSE,
+      'date_format' => 'Y-m-d',
+      'allow_date_only' => FALSE,
     );
-    $this->drupalPost('admin/config/content/scheduler', $edit, t('Save configuration'));
+    $this->drupalPostForm('admin/config/content/scheduler', $edit, t('Save configuration'));
     $this->assertRaw(t('You must either include a time within the date format or enable the date-only option.'), 'It is not possible to enter a date format without a time if the "date only" option is not enabled.');
   }
 
@@ -90,26 +94,27 @@ class SchedulerDateCombinedFunctionalTest extends SchedulerTestBase {
     $unpublish_validation_message = "The 'unpublish on' value does not match the expected format of";
 
     // First test with the "date only" functionality disabled.
-    $this->drupalPost('admin/config/content/scheduler', array('scheduler_allow_date_only' => FALSE), t('Save configuration'));
+    $this->drupalPostForm('admin/config/content/scheduler', array('allow_date_only' => FALSE), t('Save configuration'));
 
     // Test if entering a time is required.
     $edit = array(
-      'title' => $this->randomString(15),
-      'publish_on' => date('Y-m-d', strtotime('+1 day', REQUEST_TIME)),
-      'unpublish_on' => date('Y-m-d', strtotime('+2 day', REQUEST_TIME)),
+      'title[0][value]' => t('No time') . ' ' . $this->randomString(15),
+      'publish_on[0][value][date]' => date('Y-m-d', strtotime('+1 day', REQUEST_TIME)),
+      'unpublish_on[0][value][date]' => date('Y-m-d', strtotime('+2 day', REQUEST_TIME)),
     );
-    $this->drupalPost('node/add/page', $edit, t('Save'));
+    $this->drupalPostForm('node/add/page', $edit, t('Save and publish'));
 
     $this->assertRaw($publish_validation_message, 'By default it is required to enter a time when scheduling content for publication.');
     $this->assertRaw($unpublish_validation_message, 'By default it is required to enter a time when scheduling content for unpublication.');
 
     // Allow the user to enter only the date and repeat the test.
-    $this->drupalPost('admin/config/content/scheduler', array('scheduler_allow_date_only' => TRUE), t('Save configuration'));
+    $this->drupalPostForm('admin/config/content/scheduler', array('allow_date_only' => TRUE), t('Save configuration'));
 
-    $this->drupalPost('node/add/page', $edit, t('Save'));
+    $this->drupalPostForm('node/add/page', $edit, t('Save and publish'));
     $this->assertNoRaw("The 'publish on' value does not match the expected format of", 'If the default time option is enabled the user can skip the time when scheduling content for publication.');
     $this->assertNoRaw("The 'unpublish on' value does not match the expected format of", 'If the default time option is enabled the user can skip the time when scheduling content for unpublication.');
     $publish_time = date('Y-m-d H:i:s', strtotime('tomorrow', REQUEST_TIME) + 23400);
+    debug($publish_time, '$publish_time');
     $args = array('@publish_time' => $publish_time);
     $this->assertRaw(t('This post is unpublished and will be published @publish_time.', $args), 'The user is informed that the content will be published on the requested date, on the default time.');
 
