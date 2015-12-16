36c20e4206e3408b9f678aa286ef44918db0ede3
jonathan1055 <jonathan1055@92645.no-reply.drupal.org>
2015-12-16 07:45:17 +0000

    Issue #2490576 by jonathan1055, joekers: Validate that the unpublish date is in the future

--- src/Plugin/Validation/Constraint/SchedulerUnpublishOnConstraintValidator.php
+++ src/Plugin/Validation/Constraint/SchedulerUnpublishOnConstraintValidator.php
@@ -30,5 +30,14 @@ class SchedulerUnpublishOnConstraintValidator extends ConstraintValidator {
         ->atPath('unpublish_on')
         ->addViolation();
     }
+
+    // Check that the unpublish-on date is in the future. Unlike the publish-on
+    // field, there is no option to use a past date, as this is not relevant for
+    // unpublshing. The date must ALWAYS be in the future if it is entered.
+    if ($unpublish_on && $unpublish_on < REQUEST_TIME) {
+      $this->context->buildViolation($constraint->messageUnpublishOnDateNotInFuture)
+        ->atPath('unpublish_on')
+        ->addViolation();
+    }
   }
 }
