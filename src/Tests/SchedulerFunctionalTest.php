54d3793baafc2de4c39b27af179261cf6f434309
jonathan1055 <jonathan1055@92645.no-reply.drupal.org>
2016-01-16 17:59:09 +0000

    Issue #2594615: by jonathan1055: Check that fieldset is expanded if dates exist

--- src/Tests/SchedulerFunctionalTest.php
+++ src/Tests/SchedulerFunctionalTest.php
@@ -206,27 +206,62 @@ class SchedulerFunctionalTest extends SchedulerTestBase {
   }
 
   /**
-   * Tests date input is displayed as vertical tab or expandable extra fieldset.
+   * Tests date input is displayed as vertical tab or an expandable fieldset.
    */
-  public function testExtraFields() {
+  public function testFieldsDisplay() {
+    /** @var NodeTypeInterface $node_type */
     $node_type = NodeType::load('page');
     $this->drupalLogin($this->adminUser);
 
-    // Test if the options are shown as vertical tabs by default.
+    // Check that the dates are shown in a vertical tab by default.
     $this->drupalGet('node/add/page');
-    $this->assertTrue($this->xpath('//div[contains(@class, "form-type-vertical-tabs")]//details[@id = "edit-scheduler-settings"]'), 'By default the scheduler options are shown as a vertical tab.');
+    $this->assertTrue($this->xpath('//div[contains(@class, "form-type-vertical-tabs")]//details[@id = "edit-scheduler-settings"]'), 'By default the scheduler dates are shown in a vertical tab.');
 
-    // Test if the options are shown as extra fields when configured to do so.
+    // Check that the dates are shown as a fieldset when configured to do so.
     $node_type->setThirdPartySetting('scheduler', 'use_vertical_tabs', FALSE)->save();
     $this->drupalGet('node/add/page');
-    $this->assertFalse($this->xpath('//div[contains(@class, "form-type-vertical-tabs")]//details[@id = "edit-scheduler-settings"]'), 'The scheduler options are not shown as a vertical tab when they are configured to show as an extra field.');
-    $this->assertTrue($this->xpath('//details[@id = "edit-scheduler-settings" and not(@open = "open")]'), 'The scheduler options are shown as a collapsed fieldset when they are configured to show as an extra field.');
+    $this->assertFalse($this->xpath('//div[contains(@class, "form-type-vertical-tabs")]//details[@id = "edit-scheduler-settings"]'), 'The scheduler dates are not shown in a vertical tab when they are configured to show as a fieldset.');
+    $this->assertTrue($this->xpath('//details[@id = "edit-scheduler-settings" and not(@open = "open")]'), 'The scheduler dates are shown in a collapsed fieldset when they are configured to show as a fieldset.');
 
-    // Test the option to expand the fieldset.
-    $node_type->setThirdPartySetting('scheduler', 'expand_fieldset', TRUE)->save();
+    // Check that the fieldset is expanded if either of the scheduling dates
+    // are required.
+    $node_type->setThirdPartySetting('scheduler', 'publish_required', TRUE)->save();
     $this->drupalGet('node/add/page');
-    $this->assertFalse($this->xpath('//div[contains(@class, "form-type-vertical-tabs")]//details[@id = "edit-scheduler-settings"]'), 'The scheduler options are not shown as a vertical tab when they are configured to show as an expanded fieldset.');
-    $this->assertTrue($this->xpath('//details[@id = "edit-scheduler-settings" and @open = "open"]'), 'The scheduler options are shown as an expanded fieldset.');
+    $this->assertTrue($this->xpath('//details[@id = "edit-scheduler-settings" and @open = "open"]'), 'The scheduler dates are shown in an expanded fieldset when the publish-on date is required.');
+
+    $node_type->setThirdPartySetting('scheduler', 'publish_required', FALSE)
+              ->setThirdPartySetting('scheduler', 'unpublish_required', TRUE)->save();
+    $this->drupalGet('node/add/page');
+    $this->assertTrue($this->xpath('//details[@id = "edit-scheduler-settings" and @open = "open"]'), 'The scheduler dates are shown in an expanded fieldset when the unpublish-on date is required.');
+
+    // Check that the fieldset is expanded if that option is set.
+    $node_type->setThirdPartySetting('scheduler', 'publish_required', FALSE)
+              ->setThirdPartySetting('scheduler', 'unpublish_required', FALSE)
+              ->setThirdPartySetting('scheduler', 'expand_fieldset', TRUE)->save();
+    $this->drupalGet('node/add/page');
+    $this->assertTrue($this->xpath('//details[@id = "edit-scheduler-settings" and @open = "open"]'), 'The scheduler dates are shown in an expanded fieldset when the option to always expand is turned on.');
+
+    // Check that the fieldset is expanded if the node already has a publish-on
+    // date. This requires editing an existing scheduled node.
+    $node_type->setThirdPartySetting('scheduler', 'expand_fieldset', FALSE)->save();
+    $options = [
+      'title' => 'Contains Publish-on date ' . $this->randomMachineName(10),
+      'type' => 'page',
+      'publish_on' => strtotime('+1 day'),
+    ];
+    $node = $this->drupalCreateNode($options);
+    $this->drupalGet('node/' . $node->id() . '/edit');
+    $this->assertTrue($this->xpath('//details[@id = "edit-scheduler-settings" and @open = "open"]'), 'The scheduler dates are shown in an expanded fieldset when a publish-on date already exists.');
+
+    // Check that the fieldset is expanded if the node has an unpublish-on date.
+    $options = [
+      'title' => 'Contains Unpublish-on date ' . $this->randomMachineName(10),
+      'type' => 'page',
+      'unpublish_on' => strtotime('+1 day'),
+    ];
+    $node = $this->drupalCreateNode($options);
+    $this->drupalGet('node/' . $node->id() . '/edit');
+    $this->assertTrue($this->xpath('//details[@id = "edit-scheduler-settings" and @open = "open"]'), 'The scheduler dates are shown in an expanded fieldset when an unpublish-on date already exists.');
   }
 
   /**
