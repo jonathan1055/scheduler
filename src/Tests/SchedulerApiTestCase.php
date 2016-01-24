8a3f665beca69a5a18795b820bce5b8596295dc5
jonathan1055 <jonathan1055@92645.no-reply.drupal.org>
2016-01-24 13:31:31 +0000

    Issue #2594615: by jonathan1055: First part of work converting SchedulerApiTestCase to 8.x

--- src/Tests/SchedulerApiTestCase.php
+++ src/Tests/SchedulerApiTestCase.php
@@ -10,18 +10,20 @@ namespace Drupal\scheduler\Tests;
 use Drupal\Component\Utility\SafeMarkup;
 use Drupal\node\Entity\Node;
 use Drupal\simpletest\WebTestBase;
+use Drupal\node\Entity\NodeType;
 
 /**
  * Tests the API of the Scheduler module.
  *
  * @group scheduler
  */
-class SchedulerApiTestCase extends WebTestBase {
+//class SchedulerApiTestCase extends WebTestBase {
+class SchedulerApiTestCase extends SchedulerTestBase {
 
   /**
    * The modules to be loaded for these tests.
    */
-  public static $modules = array('node', 'scheduler', 'scheduler_test');
+  public static $modules = array('scheduler', 'scheduler_api_test');
 
   /**
    * The profile to install as a basis for testing.
@@ -37,54 +39,75 @@ class SchedulerApiTestCase extends WebTestBase {
     parent::setUp();
     $config = $this->config('scheduler.settings');
 
-    // Add scheduler functionality to the 'scheduler_test' node type.
-    $config
-      ->set('scheduler_publish_enable_scheduler_test', 1)
-      ->set('scheduler_unpublish_enable_scheduler_test', 1)
-      ->save();
-    debug($config, '$config');
+    // TEMP. Add these lines to send devel dd() to the usual file.     
+    debug(file_directory_temp(), 'existing file_directory_temp()');   
+    \Drupal::configFactory()->getEditable('system.file')->set('path.temporary', '/private/tmp')->save(TRUE);   
+    debug(file_directory_temp(), 'new file_directory_temp()');   
+
+    // Add scheduler functionality to the custom node type.
+    $this->nodetype = NodeType::load('scheduler_api_test');
+    if ($this->nodetype) {
+      $this->nodetype->setThirdPartySetting('scheduler', 'publish_enable', TRUE)
+        ->setThirdPartySetting('scheduler', 'unpublish_enable', TRUE)
+        ->save();
+      debug($this->nodetype->get('type'), 'nodetype->get(type)');
+      debug($this->nodetype->get('name'), 'nodetype->get(name)');
+    }
+    else {
+      $this->fail('Node type "scheduler_api_test" was not created');
+    }
+    $node_type_names = node_type_get_names();
+    debug($node_type_names, '$node_type_names'); // for debug;
+    
+    // Create an administrator user.
+    $this->adminUser = $this->drupalCreateUser(['create ' . $this->nodetype->get('type') . ' content']);
   }
 
   /**
    * Tests hook_scheduler_allow().
    *
    * This hook can allow or deny the (un)publication of individual nodes. This
-   * test uses a content type 'scheduler_test' which has a checkbox 'Approved
-   * for publication by the CEO'. Only if this is checked the node may be
-   * published.
+   * test uses a content type which has a checkbox 'Approved for publication'.
+   * The node may only be published if this checkbox is ticked.
    *
    * @todo Create and update the nodes through the interface so we can check if
    *   the correct messages are displayed.
    */
   public function testAllowedPublishing() {
-    $config = \Drupal::config('scheduler.settings');
-//    $config = $this->config('scheduler.settings');
-    // Create a node that is not approved for publication. Then simulate a cron
-    // run, and check that the node is not published.
+    // Check that the approved field is shown on the node/add form.
+    $this->drupalLogin($this->adminUser);
+    $this->drupalGet('node/add/' . $this->nodetype->get('type'));
+    $this->assertFieldByName('approved[0][value]', '', 'The Approved field is shown on the node form');
+    
+    // Create a node that is scheduled but not approved for publication. Then
+    // simulate a cron run, and check that the node is not published.
     $node = $this->createUnapprovedNode();
+    debug($node->id() . ' ' . $node->title, 'node');
     scheduler_cron();
-    $this->assertNodeNotPublished($node->id(), 'An unapproved node is not published after scheduling.');
+    $this->assertFalse($node->isPublished(), 'An unapproved node is not published after scheduling.');
 
     // Approve the node for publication, simulate a cron run, check that the
     // node is now published.
+    debug('now approving the node');
     $this->approveNode($node->id());
     scheduler_cron();
-    $this->assertNodePublished($node->id(), 'An approved node is published after scheduling.');
+    $this->assertTrue($node->isPublished(), 'An approved node is published after scheduling.');
 
     // Turn on immediate publication of nodes with publication dates in the past
     // and repeat the tests. It is not needed to simulate cron runs now.
-    $config->set('scheduler_publish_past_date_scheduler_test', 'publish');
+    debug('turn on immediate publishing');
+    $this->nodetype->setThirdPartySetting('scheduler', 'publish_past_date', 'publish')->save();
     $node = $this->createUnapprovedNode();
-    $this->assertNodeNotPublished($node->id(), 'An unapproved node is not published immediately after saving.');
+    $this->assertFalse($node->isPublished(), 'An unapproved node is not published immediately after saving.');
     $this->approveNode($node->id());
-    $this->assertNodePublished($node->id(), 'An approved node is published immediately after saving.');
+    $this->assertTrue($node->isPublished(), 'An approved node is published immediately after saving.');
   }
 
   /**
    * Creates a new node that is not approved by the CEO.
    *
    * The node has a publication date in the past to make sure it will be
-   * included in the next scheduling run.
+   * included in the next cron run.
    *
    * @return \Drupal\node\NodeInterface
    *   A node object.
@@ -93,7 +116,8 @@ class SchedulerApiTestCase extends WebTestBase {
     $settings = array(
       'status' => 0,
       'publish_on' => strtotime('-1 day'),
-      'type' => 'scheduler_test',
+      'type' => $this->nodetype->get('type'),
+      'title' => $this->randomMachineName(10),
     );
     return $this->drupalCreateNode($settings);
   }
@@ -108,61 +132,8 @@ class SchedulerApiTestCase extends WebTestBase {
     $node_storage = $this->container->get('entity.manager')->getStorage('node');
     $node_storage->resetCache(array($nid));
     $node = $node_storage->load($nid);
-    $node->set('field_scheduler_test_approved' , TRUE);
+    $node->set('field_scheduler_api_test_approved' , TRUE);
     $node->save();
-  }
-
-  /**
-   * Check to see if a node is not published.
-   *
-   * @param int $nid
-   *   The nid of the node to check.
-   * @param string $message
-   *   The message to display along with the assertion.
-   * @param string $group
-   *   The type of assertion - examples are "Browser", "PHP".
-   *
-   * @return
-   *   TRUE if the assertion succeeded, FALSE otherwise.
-   */
-  public function assertNodeNotPublished($nid, $message = NULL, $group = 'Other') {
-    $message = $message ? $message : SafeMarkup::format('Node %nid is not published', array('%nid' => $nid));
-    return $this->assertFalse($this->getPublicationStatus($nid), $message, $group);
-  }
-
-  /**
-   * Check to see if a node is published.
-   *
-   * @param int $nid
-   *   The nid of the node to check.
-   * @param string $message
-   *   The message to display along with the assertion.
-   * @param string $group
-   *   The type of assertion - examples are "Browser", "PHP".
-   *
-   * @return
-   *   TRUE if the assertion succeeded, FALSE otherwise.
-   */
-  public function assertNodePublished($nid, $message = NULL, $group = 'Other') {
-    $message = $message ? $message : SafeMarkup::format('Node %nid is published', array('%nid' => $nid));
-    return $this->assertTrue($this->getPublicationStatus($nid), $message, $group);
-  }
-
-  /**
-   * Returns the publication status of a node.
-   *
-   * @param int $nid
-   *   The nid of the node for which the publication status is desired.
-   *
-   * @return bool
-   *   TRUE if the node is published, FALSE otherwise.
-   */
-  protected function getPublicationStatus($nid) {
-    return db_select('node', 'n')
-      ->fields('n', array('status'))
-      ->condition('n.nid', $nid)
-      ->execute()
-      ->fetchColumn();
   }
 
 }
