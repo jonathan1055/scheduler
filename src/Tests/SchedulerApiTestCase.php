a4ff462fda0ee48c3579b2f2f11507327ff13838
jonathan1055 <jonathan1055@92645.no-reply.drupal.org>
2016-03-22 13:31:52 +0000

    Issue #2655666 by jonathan1055: API Testing - expanded coverage and new field

--- src/Tests/SchedulerApiTestCase.php
+++ src/Tests/SchedulerApiTestCase.php
@@ -59,26 +59,28 @@ class SchedulerApiTestCase extends SchedulerTestBase {
    * Tests hook_scheduler_allow().
    *
    * This hook can allow or deny the (un)publication of individual nodes. This
-   * test uses a content type which has a checkbox 'Approved for publication'.
-   * The node may only be published if this checkbox is ticked.
+   * test uses a content type which has checkboxes 'Approved for publication'
+   * and 'Approved for unpublication'. The node may only be published or 
+   * unpublished if the appropriate checkbox is ticked.
    *
    * @todo Create and update the nodes through the interface so we can check if
    *   the correct messages are displayed.
    */
-  public function testAllowedPublishing() {
+  public function testAllowedPublishingAndUnpublishing() {
     if (empty($this->nodetype)) {
       $this->fail('*** Custom node type ' . $this->custom_type . ' does not exist. Testing abandoned ***');
       return;
     }
 
-    // Check that the approved field is shown on the node/add form.
+    // Check that the approved fields are shown on the node/add form.
     $this->drupalLogin($this->webUser);
     $this->drupalGet('node/add/' . $this->custom_type);
-    $this->assertFieldById('edit-field-approved-value', '', 'The Approved field is shown on the node form');
+    $this->assertFieldById('edit-field-approved-publishing-value', '', 'The "Approved for publishing" field is shown on the node form');
+    $this->assertFieldById('edit-field-approved-unpublishing-value', '', 'The "Approved for unpublishing" field is shown on the node form');
 
     // Create a node that is scheduled but not approved for publication. Then
     // simulate a cron run, and check that the node is still not published.
-    $node = $this->createUnapprovedNode();
+    $node = $this->createUnapprovedNode('publish_on');
     scheduler_cron();
     $this->node_storage->resetCache(array($node->id()));
     $node = $this->node_storage->load($node->id());
@@ -86,7 +88,7 @@ class SchedulerApiTestCase extends SchedulerTestBase {
 
     // Approve the node for publication, simulate a cron run and check that the
     // node is now published.
-    $this->approveNode($node->id());
+    $this->approveNode($node->id(), 'field_approved_publishing');
     scheduler_cron();
     $this->node_storage->resetCache(array($node->id()));
     $node = $this->node_storage->load($node->id());
@@ -95,52 +97,79 @@ class SchedulerApiTestCase extends SchedulerTestBase {
     // Turn on immediate publication of nodes with publication dates in the past
     // and repeat the tests. It is not needed to simulate cron runs here.
     $this->nodetype->setThirdPartySetting('scheduler', 'publish_past_date', 'publish')->save();
-    $node = $this->createUnapprovedNode();
+    $node = $this->createUnapprovedNode('publish_on');
     $this->assertFalse($node->isPublished(), 'An unapproved node with a date in the past is not published immediately after saving.');
 
     // Check that the node can be approved and published programatically.
-    $this->approveNode($node->id());
+    $this->approveNode($node->id(), 'field_approved_publishing');
     $this->node_storage->resetCache(array($node->id()));
     $node = $this->node_storage->load($node->id());
     $this->assertTrue($node->isPublished(), 'An approved node with a date in the past is published immediately via $node->set()->save().');
 
     // Check that a node can be approved and published via edit form.
-    $node = $this->createUnapprovedNode();
-    $this->drupalPostForm('node/' . $node->id() . '/edit', ['field_approved[value]' => '1'], t('Save'));
+    $node = $this->createUnapprovedNode('publish_on');
+    $this->drupalPostForm('node/' . $node->id() . '/edit', ['field_approved_publishing[value]' => '1'], t('Save'));
     $this->node_storage->resetCache(array($node->id()));
     $node = $this->node_storage->load($node->id());
     $this->assertTrue($node->isPublished(), 'An approved node with a date in the past is published immediately after saving via edit form.');
+
+    // Test approval for unpublishing. This is simpler than the test sequence
+    // for publishing, because the 'immediate' option is not applicable.
+
+    // Create a node that is scheduled but not approved for unpublication. Then
+    // simulate a cron run, and check that the node is still published.
+    $node = $this->createUnapprovedNode('unpublish_on');
+    scheduler_cron();
+    $this->node_storage->resetCache(array($node->id()));
+    $node = $this->node_storage->load($node->id());
+    $this->assertTrue($node->isPublished(), 'An unapproved node is not unpublished during cron processing.');
+
+    // Approve the node for unpublishing, simulate a cron run and check that
+    // the node is now unpublished.
+    $this->approveNode($node->id(), 'field_approved_unpublishing');
+    scheduler_cron();
+    $this->node_storage->resetCache(array($node->id()));
+    $node = $this->node_storage->load($node->id());
+    $this->assertFalse($node->isPublished(), 'An approved node is unpublished during cron processing.');
   }
 
   /**
-   * Creates a new node that is not approved by the CEO.
+   * Creates a new node that is not approved.
    *
-   * The node has a publication date in the past to make sure it will be
+   * The node has a publish/unpublish date in the past to make sure it will be
    * included in the next cron run.
+   *
+   * @param string $date_field
+   *   The Scheduler date field to set, either 'publish_on' or 'unpublish_on'.
    *
    * @return \Drupal\node\NodeInterface
    *   A node object.
    */
-  protected function createUnapprovedNode() {
+  protected function createUnapprovedNode($date_field) {
     $settings = array(
-      'status' => 0,
-      'publish_on' => strtotime('-1 day'),
-      'field_approved' => 0,
+      'status' => ($date_field == 'unpublish_on'),
+      $date_field => strtotime('-1 day'),
+      'field_approved_publishing' => 0,
+      'field_approved_unpublishing' => 0,
       'type' => $this->custom_type,
     );
     return $this->drupalCreateNode($settings);
   }
 
   /**
-   * Approves a node for publication.
+   * Approves a node for publication or unpublication.
    *
    * @param int $nid
    *   The id of the node to approve.
+   *
+   * @param string $field_name
+   *   The name of the field to set, either 'field_approved_publishing' or
+   *   'field_approved_for_unpublishing'.
    */
-  protected function approveNode($nid) {
+  protected function approveNode($nid, $field_name) {
     $this->node_storage->resetCache(array($nid));
     $node = $this->node_storage->load($nid);
-    $node->set('field_approved' , TRUE)->save();
+    $node->set($field_name, TRUE)->save();
   }
 
 }
