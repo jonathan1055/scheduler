8cc62c1a8a7863789567ccf0a7101028f61f3a56
jonathan1055 <jonathan1055@92645.no-reply.drupal.org>
2016-03-26 14:18:31 +0000

    Issue #2633870 by jonathan1055, pfrenssen: Constants as defaults for third party settings (the remaining seven)

--- src/Plugin/Validation/Constraint/SchedulerUnpublishOnConstraintValidator.php
+++ src/Plugin/Validation/Constraint/SchedulerUnpublishOnConstraintValidator.php
@@ -19,7 +19,7 @@ class SchedulerUnpublishOnConstraintValidator extends ConstraintValidator {
    * {@inheritdoc}
    */
   public function validate($entity, Constraint $constraint) {
-    $scheduler_unpublish_required = $entity->getEntity()->type->entity->getThirdPartySetting('scheduler', 'unpublish_required');
+    $scheduler_unpublish_required = $entity->getEntity()->type->entity->getThirdPartySetting('scheduler', 'unpublish_required', SCHEDULER_DEFAULT_UNPUBLISH_REQUIRED);
     $publish_on = $entity->getEntity()->publish_on->value;
     $unpublish_on = $entity->value;
 
