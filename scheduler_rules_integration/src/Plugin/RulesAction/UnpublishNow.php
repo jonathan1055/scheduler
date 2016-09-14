ceede1e9dd4d7a4f800725fca43fdbdab218752c
jonathan1055 <jonathan1055@92645.no-reply.drupal.org>
2016-09-14 15:59:15 +0100

    Issue #2651348 by jonathan1055: Add rules actions to publish and unpublish now

---
+++ scheduler_rules_integration/src/Plugin/RulesAction/UnpublishNow.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Drupal\scheduler_rules_integration\Plugin\RulesAction;
+
+use Drupal\rules\Core\RulesActionBase;
+
+/**
+ * Provides an 'Unpublish the node immediately' action.
+ *
+ * @RulesAction(
+ *   id = "scheduler_unpublish_now_action",
+ *   label = @Translation("Unpublish the content immediately"),
+ *   category = @Translation("Scheduler"),
+ *   context = {
+ *     "node" = @ContextDefinition("entity:node",
+ *       label = @Translation("Node"),
+ *       description = @Translation("The node to be unpublished now"),
+ *     ),
+ *   }
+ * )
+ */
+class UnpublishNow extends RulesActionBase {
+
+  /**
+   * Set the node status to Unpublished.
+   *
+   * @param \Drupal\node\Entity\Node $node
+   *   The node object which will be set to Unpublished.
+   *
+   * This action should really be provided by Rules or by Core, but it is not
+   * yet done (as of Aug 2016). Scheduler users need this action so we provide
+   * it here. It could be removed later when Rules or Core includes it.
+   */
+  public function doExecute() {
+    $node = $this->getContextValue('node');
+    $node->setPublished(FALSE);
+  }
+}
