a88ca7a1dc12d27449745bab588b928bba37f1c9
jonathan1055 <jonathan1055@92645.no-reply.drupal.org>
2017-06-12 15:21:56 +0100

    Issue #2712465 by jonathan1055: Remove direct DB code and tidy up BasicTest

--- tests/src/Functional/SchedulerBasicTest.php
+++ tests/src/Functional/SchedulerBasicTest.php
@@ -13,15 +13,15 @@ class SchedulerBasicTest extends SchedulerBrowserTestBase {
    * Tests basic scheduling of content.
    */
   public function testPublishingAndUnpublishing() {
-    // Login to admin user. This is required here before creating the publish_on
-    // date and time values so that date.formatter can utilise the current users
-    // timezone. The constraints receive values which have been converted using
-    // the users timezone so they need to be consistent.
-    $this->drupalLogin($this->adminUser);
+    // Login is required here before creating the publish_on date and time
+    // values so that date.formatter can utilise the current users timezone. The
+    // constraints receive values which have been converted using the users
+    // timezone so they need to be consistent.
+    $this->drupalLogin($this->schedulerUser);
 
     // Create node values. Set time to one hour in the future.
     $edit = [
-      'title[0][value]' => 'Publishing ' . $this->randomMachineName(10),
+      'title[0][value]' => 'Publish This Node',
       'publish_on[0][value][date]' => \Drupal::service('date.formatter')->format(time() + 3600, 'custom', 'Y-m-d'),
       'publish_on[0][value][time]' => \Drupal::service('date.formatter')->format(time() + 3600, 'custom', 'H:i:s'),
     ];
@@ -33,24 +33,20 @@ class SchedulerBasicTest extends SchedulerBrowserTestBase {
     unset($edit['publish_on[0][value][date]']);
     unset($edit['publish_on[0][value][time]']);
     // Need a new title for the new node, as we identify the node by title.
-    $edit['title[0][value]'] = 'Unpublishing ' . $this->randomMachineName(10);
+    $edit['title[0][value]'] = 'Unpublish This Node';
     $this->helpTestScheduler($edit);
   }
 
   /**
-   * Helper function for testScheduler(). Schedules content and asserts status.
+   * Helper function for testPublishingAndUnpublishing().
+   *
+   * Schedules content, runs cron and asserts status.
    */
   protected function helpTestScheduler($edit) {
-    // Add a page, using randomMachineName for the body text, not randomString,
-    // because assertText works better without difficult non-alpha characters.
-    $body = $this->randomMachineName(30);
-
-    $edit['body[0][value]'] = $body;
-    $this->drupalLogin($this->schedulerUser);
     $this->drupalPostForm('node/add/' . $this->type, $edit, t('Save'));
     // Verify that the node was created.
     $node = $this->drupalGetNodeByTitle($edit['title[0][value]']);
-    $this->assertTrue($node, sprintf('Node for %s was created sucessfully.', $edit['title[0][value]']));
+    $this->assertTrue($node, sprintf('"%s" was created sucessfully.', $edit['title[0][value]']));
     if (empty($node)) {
       $this->assert(FALSE, 'Test halted because node was not created.');
       return;
@@ -67,16 +63,14 @@ class SchedulerBasicTest extends SchedulerBrowserTestBase {
     }
 
     // Modify the scheduler field data to a time in the past, then run cron.
-    // @TODO change this to node_save()
-    $this->database->update('node_field_data')->fields([$key => time() - 1])->condition('nid', $node->id())->execute();
-    $this->database->update('node_field_revision')->fields([$key => time() - 1])->condition('nid', $node->id())->execute();
-    $this->nodeStorage->resetCache([$node->id()]);
-
+    $node->$key = REQUEST_TIME - 1;
+    $node->save();
     $this->cronRun();
 
-    // Assert that the node is correctly published or unpublished.
+    // Refresh the node cache and check the node status.
+    $this->nodeStorage->resetCache([$node->id()]);
     $node = $this->nodeStorage->load($node->id());
-    if (isset($edit['publish_on[0][value][date]'])) {
+    if ($key == 'publish_on') {
       $this->assertTrue($node->isPublished(), 'Node is published after Cron');
     }
     else {
