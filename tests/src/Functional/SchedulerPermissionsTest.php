b765ff4c5ab7a78a15731761b3531588108ea46f
shubham.prakash <shubham.prakash@3608696.no-reply.drupal.org>
2019-11-06 13:17:56 +0000

    Issue #3084931 by shubham.prakash, Sahana _N, jonathan1055: Remove calls to deprecated method assertEqual() and replace with assertEquals()

--- tests/src/Functional/SchedulerPermissionsTest.php
+++ tests/src/Functional/SchedulerPermissionsTest.php
@@ -108,7 +108,7 @@ class SchedulerPermissionsTest extends SchedulerBrowserTestBase {
     ]);
 
     // Verify that the publish_on date is stored as expected before editing.
-    $this->assertEqual($unpublished_node->publish_on->value, $publish_time, 'The publish_on value is stored correctly before edit.');
+    $this->assertEquals($publish_time, $unpublished_node->publish_on->value, 'The publish_on value is stored correctly before edit.');
 
     // Edit the unpublished node and save.
     $title = 'For Publishing ' . $this->randomString(10);
@@ -116,14 +116,14 @@ class SchedulerPermissionsTest extends SchedulerBrowserTestBase {
 
     // Check the updated title, to verify that edit and save was sucessful.
     $unpublished_node = $this->nodeStorage->load($unpublished_node->id());
-    $this->assertEqual($unpublished_node->title->value, $title, 'The unpublished node title has been updated correctly after edit.');
+    $this->assertEquals($title, $unpublished_node->title->value, 'The unpublished node title has been updated correctly after edit.');
 
     // Test that the publish_on date is still stored and is unchanged.
-    $this->assertEqual($unpublished_node->publish_on->value, $publish_time, 'The node publish_on value is still stored correctly after edit.');
+    $this->assertEquals($publish_time, $unpublished_node->publish_on->value, 'The node publish_on value is still stored correctly after edit.');
 
     // Do the same for unpublishing.
     // Verify that the unpublish_on date is stored as expected before editing.
-    $this->assertEqual($published_node->unpublish_on->value, $unpublish_time, 'The unpublish_on value is stored correctly before edit.');
+    $this->assertEquals($unpublish_time, $published_node->unpublish_on->value, 'The unpublish_on value is stored correctly before edit.');
 
     // Edit the published node and save.
     $title = 'For Unpublishing ' . $this->randomString(10);
@@ -131,10 +131,10 @@ class SchedulerPermissionsTest extends SchedulerBrowserTestBase {
 
     // Check the updated title, to verify that edit and save was sucessful.
     $published_node = $this->nodeStorage->load($published_node->id());
-    $this->assertEqual($published_node->title->value, $title, 'The published node title has been updated correctly after edit.');
+    $this->assertEquals($title, $published_node->title->value, 'The published node title has been updated correctly after edit.');
 
     // Test that the unpublish_on date is still stored and is unchanged.
-    $this->assertEqual($published_node->unpublish_on->value, $unpublish_time, 'The node unpublish_on value is still stored correctly after edit.');
+    $this->assertEquals($unpublish_time, $published_node->unpublish_on->value, 'The node unpublish_on value is still stored correctly after edit.');
 
   }
 
