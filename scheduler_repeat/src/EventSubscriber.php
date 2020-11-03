277e9af6251835898e6923b7538dd578cbd30f50
Jonathan Smith <jonathan1055@sandfordsolutions.com>
2020-11-03 12:09:21 +0000

    Remove "next" calculations from event unpublish()

--- scheduler_repeat/src/EventSubscriber.php
+++ scheduler_repeat/src/EventSubscriber.php
@@ -53,21 +53,9 @@ class EventSubscriber implements EventSubscriberInterface {
       // Do not have both values, so cannot set the next period.
       return;
     }
+    // Set the new period dates.
     $node->set('publish_on', $next_publish_on);
     $node->set('unpublish_on', $next_unpublish_on);
-
-    // Calculate and store the new next_publish_on and next_unpublish_on values.
-    $request_time = \Drupal::time()->getRequestTime();
-    while ($next_publish_on < $request_time) {
-      $next_publish_on = $repeater->calculateNextPublishedOn($next_publish_on);
-      $next_unpublish_on = $repeater->calculateNextUnpublishedOn($next_unpublish_on);
-    }
-    $node->set('scheduler_repeat', [
-      'plugin' => $node->scheduler_repeat->plugin,
-      'next_publish_on' => $next_publish_on,
-      'next_unpublish_on' => $next_unpublish_on,
-    ]);
-
     $event->setNode($node);
   }
 }
