778e02e37a92701c5c043f4f6e9e8d73dcd08de4
Martin Gottlieb <marting@cobblestone.consulting>
2020-12-29 16:13:17 -0500

    Code cleanup

--- src/SchedulerPluginBase.php
+++ src/SchedulerPluginBase.php
@@ -4,27 +4,51 @@ namespace Drupal\scheduler;
 
 use Drupal\Core\Plugin\PluginBase;
 
+/**
+ * Abstract class for scheduler plugins.
+ */
 abstract class SchedulerPluginBase extends PluginBase implements SchedulerPluginInterface {
 
   /**
    * @inheritDoc
    */
-  public function description()
-  {
+  public function description() {
     return $this->pluginDefinition['description'];
   }
 
+  /**
+   * Get plugin label.
+   *
+   * @return string
+   *   The label.
+   */
   public function label() {
     return $this->pluginDefinition['label'];
   }
 
+  /**
+   * Publish pending entities.
+   */
   abstract public function publish();
 
+  /**
+   * Unpublish pending entities.
+   */
   abstract public function unpublish();
 
+  /**
+   * Get the bundle name for entity.
+   */
   abstract public function entityType();
 
+  /**
+   * Get the form IDs for entity add/edit forms.
+   */
   abstract public function entityFormIds();
 
+  /**
+   * Get the list of entity type form IDs.
+   */
   abstract public function entityTypeFormIds();
+
 }
