5a04e8e2459603132f253f2e0ac8834f7469d793
Martin Gottlieb <marting@cobblestone.consulting>
2020-12-30 09:53:30 -0500

    Code cleanup

--- src/SchedulerPluginBase.php
+++ src/SchedulerPluginBase.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\scheduler;
 
+use Drupal\Core\Entity\EntityInterface;
 use Drupal\Core\Plugin\PluginBase;
 
 /**
@@ -10,6 +11,8 @@ use Drupal\Core\Plugin\PluginBase;
 abstract class SchedulerPluginBase extends PluginBase implements SchedulerPluginInterface {
 
   /**
+   * Description of plugin.
+   *
    * @inheritDoc
    */
   public function description() {
@@ -50,5 +53,24 @@ abstract class SchedulerPluginBase extends PluginBase implements SchedulerPlugin
    * Get the list of entity type form IDs.
    */
   abstract public function entityTypeFormIds();
+
+  /**
+   * Get list of Entity Type objects.
+   *
+   * @return array
+   *   The list of entity type objects.
+   */
+  abstract public function getTypes();
+
+  /**
+   * Return the entity type object for a specific entity.
+   *
+   * @param EntityInterface $entity
+   *   The entity.
+   *
+   * @return mixed
+   *   The entity type object.
+   */
+  abstract public function getEntityType(EntityInterface $entity);
 
 }
