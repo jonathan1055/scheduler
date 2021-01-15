acd73dc76f0a4a99059f47f514366c70dc614119
Martin Gottlieb <marting@cobblestone.consulting>
2021-01-15 10:21:18 -0500

    Add method stubs to plugin interface

--- src/SchedulerPluginInterface.php
+++ src/SchedulerPluginInterface.php
@@ -23,4 +23,72 @@ interface SchedulerPluginInterface {
    */
   public function label();
 
+  /**
+   * Get the type of entity supported by this plugin.
+   *
+   * @return string
+   *   The name of the entity type.
+   */
+  public function entityType();
+
+  /**
+   * Get module dependency.
+   *
+   * @return string
+   *   The name of the required module.
+   */
+  public function dependency();
+
+  /**
+   * Get the name of the "type" field for the entity.
+   *
+   * @return string
+   *   The name of the type/bundle field for this entity type.
+   */
+  public function typeFieldName();
+
+  /**
+   * Get the name of the id list hook function.
+   *
+   * Implementations of this hook function allow other modules to add to the
+   * list of ids being published or unpublished. Further, implementations of
+   * it's corresponding 'alter' function allow full manipulation of the list,
+   * for adding and removing ids.
+   *
+   * @return string
+   *   The hook function name.
+   */
+  public function idListFunction();
+
+  /**
+   * Get the id of the Devel Generate form for this entity type.
+   *
+   * @return string
+   *   The form id.
+   */
+  public function develGenerateForm();
+
+  /**
+   * Get the form IDs for entity add/edit forms.
+   */
+  public function entityFormIds();
+
+  /**
+   * Get all the entity type/bundle objects.
+   *
+   * @return array
+   *   The entity type objects.
+   */
+  public function getTypes();
+
+  /**
+   * Get all types/bundles enabled for the specified $action.
+   *
+   * @param string $action
+   *   The action - publish|unpublish.
+   *
+   * @return array
+   *   The type/bundle objects.
+   */
+  public function getEnabledTypes($action);
 }
