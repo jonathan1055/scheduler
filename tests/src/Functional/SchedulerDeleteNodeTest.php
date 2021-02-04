deabb7835a80a912f378d72eb1cb60fb9924c1aa
Jonathan Smith <jonathan1055@sandfordsolutions.com>
2021-02-04 13:49:34 +0000

    Expand DeleteNode test to cover Media

--- tests/src/Functional/SchedulerDeleteNodeTest.php
+++ tests/src/Functional/SchedulerDeleteNodeTest.php
@@ -3,9 +3,9 @@
 namespace Drupal\Tests\scheduler\Functional;
 
 /**
- * Tests deletion of nodes enabled for Scheduler.
+ * Tests deletion of entities enabled for Scheduler.
  *
- * This checks how the deletion of a node interacts with the Scheduler
+ * This checks how the deletion of an entity interacts with the Scheduler
  * 'required' options and scheduled dates in the past.
  *
  * @group scheduler
@@ -13,84 +13,101 @@ namespace Drupal\Tests\scheduler\Functional;
 class SchedulerDeleteNodeTest extends SchedulerBrowserTestBase {
 
   /**
-   * Tests the deletion of a scheduled node.
+   * Tests the deletion of an entity when the scheduler dates are required.
    *
-   * Check that it is possible to delete a node that does not have a publishing
-   * date set, when scheduled publishing is required. Likewise for unpublishing.
+   * Check that it is possible to delete an entity that does not have a
+   * publishing date set, when scheduled publishing is required.
+   * Likewise for unpublishing.
    *
-   * @see https://drupal.org/node/1614880
+   * @see https://www.drupal.org/project/scheduler/issues/1614880
+   *
+   * @dataProvider dataDeleteEntity()
    */
-  public function testDeleteNodeWhenSchedulingIsRequired() {
+  public function testDeleteEntityWhenSchedulingIsRequired($entityType, $bundle, $typeFieldName) {
     // Log in.
     $this->drupalLogin($this->adminUser);
 
-    // Create a published and an unpublished node, both without scheduled dates.
-    $published_node = $this->drupalCreateNode([
-      'type' => $this->type,
-      'status' => 1,
+    // Create a published and an unpublished entity, with no scheduled dates.
+    $published_entity = $this->createEntity($entityType, $bundle, [
+      'status' => TRUE,
     ]);
-    $unpublished_node = $this->drupalCreateNode([
-      'type' => $this->type,
-      'status' => 0,
+    $unpublished_entity = $this->createEntity($entityType, $bundle, [
+      'status' => FALSE,
     ]);
 
     // Make scheduled publishing and unpublishing required.
-    $this->nodetype->setThirdPartySetting('scheduler', 'publish_required', TRUE)
+    $published_entity->$typeFieldName->entity->setThirdPartySetting('scheduler', 'publish_required', TRUE)
       ->setThirdPartySetting('scheduler', 'unpublish_required', TRUE)
       ->save();
+    $entity_type_label = $published_entity->getEntityType()->getSingularLabel();
 
-    // Check that deleting the nodes does not throw form validation errors.
-    $this->drupalGet('node/' . $published_node->id() . '/edit');
+    // Check that deleting the entity does not throw form validation errors.
+    $this->drupalGet("$entityType/{$published_entity->id()}/edit");
     $this->clickLink('Delete');
     // The text 'error message' is used in a header h2 html tag which is
     // normally made hidden from browsers but will be in the page source.
     // It is also good when testing for the absense of something to also test
     // for the presence of text, hence the second assertion for each check.
     $this->assertSession()->pageTextNotContains('Error message');
-    $this->assertSession()->pageTextContains('Are you sure you want to delete the content');
+    $this->assertSession()->pageTextContains("Are you sure you want to delete the $entity_type_label {$published_entity->label()}");
 
-    // Do the same test for the unpublished node.
-    $this->drupalGet('node/' . $unpublished_node->id() . '/edit');
+    // Do the same test for the unpublished entity.
+    $this->drupalGet("$entityType/{$unpublished_entity->id()}/edit");
     $this->clickLink('Delete');
     $this->assertSession()->pageTextNotContains('Error message');
-    $this->assertSession()->pageTextContains('Are you sure you want to delete the content');
+    $this->assertSession()->pageTextContains("Are you sure you want to delete the $entity_type_label {$unpublished_entity->label()}");
   }
 
   /**
-   * Tests the deletion of a scheduled node.
+   * Tests the deletion of scheduled entities.
    *
-   * Check that nodes can be deleted with no validation errors if the dates are
-   * in the past.
+   * Check that entities can be deleted with no validation errors even if the
+   * dates are in the past.
    *
-   * @see http://drupal.org/node/2627370
+   * @see https://www.drupal.org/project/scheduler/issues/2627370
+   *
+   * @dataProvider dataDeleteEntity()
    */
-  public function testDeleteNodeWithPastDates() {
+  public function testDeleteEntityWithPastDates($entityType, $bundle, $typeFieldName) {
     // Log in.
     $this->drupalLogin($this->adminUser);
 
-    // Create nodes with publish_on and unpublish_on dates in the past.
-    $published_node = $this->drupalCreateNode([
-      'type' => $this->type,
+    // Create entities with publish_on and unpublish_on dates in the past.
+    $published_entity = $this->createEntity($entityType, $bundle, [
       'status' => TRUE,
       'unpublish_on' => strtotime('- 2 day'),
     ]);
-    $unpublished_node = $this->drupalCreateNode([
-      'type' => $this->type,
+    $unpublished_entity = $this->createEntity($entityType, $bundle, [
       'status' => FALSE,
       'publish_on' => strtotime('- 2 day'),
     ]);
+    $entity_type_label = $published_entity->getEntityType()->getSingularLabel();
 
-    // Attempt to delete the published node and check for no validation error.
-    $this->drupalGet('node/' . $published_node->id() . '/edit');
+    // Attempt to delete the published entity and check for no validation error.
+    $this->drupalGet("$entityType/{$published_entity->id()}/edit");
     $this->clickLink('Delete');
     $this->assertSession()->pageTextNotContains('Error message');
-    $this->assertSession()->pageTextContains('Are you sure you want to delete the content');
+    $this->assertSession()->pageTextContains("Are you sure you want to delete the $entity_type_label {$published_entity->label()}");
 
-    // Attempt to delete the unpublished node and check for no validation error.
-    $this->drupalGet('node/' . $unpublished_node->id() . '/edit');
+    // Attempt to delete the unpublished entity and check no validation error.
+    $this->drupalGet("$entityType/{$unpublished_entity->id()}/edit");
     $this->clickLink('Delete');
     $this->assertSession()->pageTextNotContains('Error message');
-    $this->assertSession()->pageTextContains('Are you sure you want to delete the content');
+    $this->assertSession()->pageTextContains("Are you sure you want to delete the $entity_type_label {$unpublished_entity->label()}");
+  }
+
+  /**
+   * Provides data for both of the test functions in this class.
+   *
+   * @return array
+   *   Each array item has the values: [entity type, bundle id, type fieldname].
+   */
+  public function dataDeleteEntity() {
+    $data = [
+      'Content' => ['node', 'testpage', 'type'],
+      'Media' => ['media', 'test_media_image', 'bundle'],
+    ];
+    return $data;
   }
 
 }
