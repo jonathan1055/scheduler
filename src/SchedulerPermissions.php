3b9cef2041c7f02e60441bcf4e8690de4370fd05
Jonathan Smith <jonathan1055@sandfordsolutions.com>
2021-03-04 16:28:27 +0000

    Create all entity permissions in SchedulerPermissions class

--- src/SchedulerPermissions.php
+++ src/SchedulerPermissions.php
@@ -3,11 +3,12 @@
 namespace Drupal\scheduler;
 
 use Drupal\Core\DependencyInjection\ContainerInjectionInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\StringTranslation\StringTranslationTrait;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
- * Provides dynamic permisions for scheduler plugins.
+ * Provides dynamic permissions for scheduler plugins.
  */
 class SchedulerPermissions implements ContainerInjectionInterface {
 
@@ -21,17 +22,30 @@ class SchedulerPermissions implements ContainerInjectionInterface {
   private $schedulerManager;
 
   /**
-   * Constructs a \Drupal\scheduler\SchedulerPermissions instance.
+   * The entity type manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
    */
-  public function __construct(SchedulerManager $schedulerManager) {
-    $this->schedulerManager = $schedulerManager;
+  protected $entityTypeManager;
+
+  /**
+   * Constructs a \Drupal\scheduler\SchedulerPermissions instance.
+   *
+   * @param \Drupal\scheduler\SchedulerManager $scheduler_manager
+   *   The entity type manager.
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   The entity type manager.
+   */
+  public function __construct(SchedulerManager $scheduler_manager, EntityTypeManagerInterface $entity_type_manager) {
+    $this->schedulerManager = $scheduler_manager;
+    $this->entityTypeManager = $entity_type_manager;
   }
 
   /**
    * {@inheritDoc}
    */
   public static function create(ContainerInterface $container) {
-    return new static($container->get('scheduler.manager'));
+    return new static($container->get('scheduler.manager'), $container->get('entity_type.manager'));
   }
 
   /**
@@ -44,16 +58,33 @@ class SchedulerPermissions implements ContainerInjectionInterface {
    */
   public function permissions() {
     $permissions = [];
-    $plugins = $this->schedulerManager->getPluginEntityTypes();
-    foreach ($plugins as $type) {
-      // Nodes are already handled in permissions.yml.
-      if ($type == 'node') {
-        continue;
+    $types = $this->schedulerManager->getPluginEntityTypes();
+    foreach ($types as $entity_type_id) {
+      $entity_type = $this->entityTypeManager->getDefinition($entity_type_id);
+      // For backwards-compatibility with existing permissions, the Node perm
+      // names have to end with 'nodes' and 'content'. For all other plugins we
+      // use $entity_type_id for both permissions.
+      if ($entity_type_id == 'node') {
+        $edit_key = 'nodes';
+        $view_key = 'content';
       }
+      else {
+        $edit_key = $view_key = $entity_type_id;
+      }
+      $t_args = [
+        '%label' => $entity_type->getLabel(),
+        '%singular_label' => $entity_type->getSingularLabel(),
+        '%plural_label' => $entity_type->getPluralLabel(),
+      ];
+
       $permissions += [
-        'schedule publishing of ' . $type => [
-          'title' => $this->t('Schedule publishing of %type', ['%type' => ucfirst($type)]),
-          'description' => $this->t('Allows users to set a start and end time for %type publication', ['%type' => ucfirst($type)]),
+        "schedule publishing of $edit_key"  => [
+          'title' => $this->t('Schedule publishing and unpublishing of %label', $t_args),
+          'description' => $this->t('Allows users to set a start and end time for %singular_label publication.', $t_args),
+        ],
+        "view scheduled $view_key" => [
+          'title' => $this->t('View scheduled %label', $t_args),
+          'description' => $this->t('Allows users to see a list of all %plural_label that are scheduled.', $t_args),
         ],
       ];
     }
